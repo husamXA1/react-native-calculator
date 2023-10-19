012b3a9f4f10ac4d1d3bf065206476daabea3196
Husam Mohamed <husamug@gmail.com>
2023-10-19 14:59:55 +0200

    created dark mode button and BG color

--- App.js
+++ App.js
@@ -1,11 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from "react";
+import { Ionicons } from "@expo/vector-icons";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  TouchableOpacity,
+  Image,
+  StatusBar,
+} from "react-native";
+
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import COLORS from "./constants/COLORS";
 
 export default function App() {
+  const [darkMode, setDarkMode] = useState(false);
+
+  useEffect(() => {
+    async function aquireDarkMode() {
+      const storageDarkMode = await AsyncStorage.getItem("darkMode");
+      if (storageDarkMode) {
+        setDarkMode(storageDarkMode === "true" ? true : false);
+      }
+    }
+    aquireDarkMode();
+  }, []);
+
+  async function switchTheme() {
+    await AsyncStorage.setItem("darkMode", (!darkMode).toString());
+    setDarkMode((pendingMode) => !pendingMode);
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View
+      style={{
+        ...styles.container,
+        backgroundColor: darkMode ? COLORS.black : COLORS.lightBG,
+      }}
+    >
+      <TouchableOpacity style={styles.darkModeIcon} onPress={switchTheme}>
+        {darkMode ? (
+          <Ionicons name="ios-sunny" size={30} color={COLORS.white} />
+        ) : (
+          <Ionicons name="ios-moon" size={30} color={COLORS.black} />
+        )}
+      </TouchableOpacity>
     </View>
   );
 }
@@ -13,8 +52,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: StatusBar.currentHeight,
+  },
+  darkModeIcon: {
+    position: "absolute",
+    top: 20,
+    left: 20,
+    width: 30,
+    height: 30,
   },
 });
---
+++ constants/COLORS.js
@@ -0,0 +1,10 @@
+export default {
+  white: "#E2E2E2",
+  offWhite: "#D2D2D2",
+  black: "#2E2E2E",
+  secondaryBlack: "#3E3E3E",
+  lightBG: "#EBE4D1",
+  secondaryLightBG: "#D2CBB8",
+  orange: "#E55604",
+  blue: "#26577C",
+};
